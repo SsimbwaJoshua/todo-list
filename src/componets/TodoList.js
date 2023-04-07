654b503e1f1a6650f43953cd008b3b02d94afcd0
SsimbwaJoshua <ssimbwajoshua124@gmail.com>
2023-04-07 12:15:45 +0300

    third commit

--- src/componets/TodoList.js
+++ src/componets/TodoList.js
@@ -13,9 +13,21 @@ export const TodoList = () => {
 
   const submitTodo = (event) => {
     event.preventDefault();
-    console.log("submit is now true");
-    (message) => {};
-    console.log(list);
+
+    message = {
+      id: list.length++,
+      text: { message },
+    };
+
+    const newList = [...list, message];
+    setList(newList);
+  };
+
+  //logic for removing task
+  const removeTask = () => {
+    console.log("clicked");
+
+    const newList = list.filter();
   };
 
   return (
@@ -23,16 +35,41 @@ export const TodoList = () => {
       <h1>THINGS TO DO</h1>
       <form onSubmit={submitTodo}>
         <input
+          style={{ width: "60%", height: "30px" }}
           placeholder="Add New"
           type="text"
           value={message}
           onChange={handleChange}
         />
-        <input type="submit" />
-        <br />
+        <input style={{ height: "30px" }} type="submit" />
       </form>
+      <br />
+      {list.map((task, key) => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              margin: "0 50px ",
 
-      <ul>{list}</ul>
+              border: "2px solid #ccc",
+              padding: "2px",
+            }}
+          >
+            <div style={{ display: "flex", gap: "10px" }}>
+              <input type="checkbox" />
+              <h1>{task}</h1>
+            </div>
+            <button
+              style={{ width: "30px", height: "30px" }}
+              onClick={removeTask}
+            >
+              X
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 };
