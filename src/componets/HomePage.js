0ea00094eb812687d7b947cb3aa075f28e067bbc
SsimbwaJoshua <ssimbwajoshua124@gmail.com>
2023-04-20 12:38:25 +0300

    media querries

--- src/componets/HomePage.js
+++ src/componets/HomePage.js
@@ -7,26 +7,28 @@ import { NavBar } from "./NavBar";
 export const HomePage = () => {
   return (
     <div className="the-whole-homepage">
-      <div className="home">
-        <header className="home-header">
+      <div className="home-home">
+        <div className="howitWorks-navigation">
           <h1>THE TODO LIST APP</h1>
-          <div className="home-header-text">
-            <h2>The todo app for your Needs</h2>
-            <p>
-              Welcome to the todo list app. Note down your daily tasks and
-              accomplish your daily goals in an orderly manner.
-            </p>
-            <br />
-            <Link to="/todolist" className="link">
-              Get Started
-            </Link>
-          </div>
-        </header>
-        <section className="home-section">
-          <div className="home-nav">
-            <NavBar />
-          </div>
-        </section>
+          <NavBar />
+        </div>
+        <div className="home">
+          <div></div>
+          <header className="home-header">
+            <div className="home-header-text">
+              <h2>The todo app for your Needs</h2>
+              <p>
+                Welcome to the todo list app. Note down your daily tasks and
+                accomplish your daily goals in an orderly manner.
+              </p>
+              <br />
+              <Link to="/todolist" className="link">
+                Get Started
+              </Link>
+            </div>
+            <div></div>
+          </header>
+        </div>
       </div>
     </div>
   );
